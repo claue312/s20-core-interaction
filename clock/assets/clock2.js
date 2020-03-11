a671b0b045eed0d9a80a35a308ec6e96e6101e42
Erin McGee <claue312@newschool.edu>
2020-03-10 21:43:38 -0400

    uploaded clock finally

--- clock/assets/clock2.js
+++ clock/assets/clock2.js
@@ -1,24 +1,66 @@
-// total height of time section
-	var times = document.getElementById("times")
-	// var progress = document.getElementById("progress") //tried this for fun //
-	var body = document.getElementsByTagName('body')[0]
-
-	var timesTotalHeight = times.offsetHeight;
+// // total height of time section
+// 	var times = document.getElementById("times")
+// 	var timesTotalHeight = times.offsetHeight;
+// 	// var delay = times.keyframes.timesTotalHeight;
 	
-	setInterval(function(){
-		var myDate = new Date;
-		var hoursInSeconds = myDate.getHours()*60*60;
-		var minutesInSeconds = myDate.getMinutes()*60;
-		var seconds = myDate.getSeconds() + minutesInSeconds + hoursInSeconds;
+// 	setInterval(function(){
+// 		var myDate = new Date;
+// 		var hoursInSeconds = myDate.getHours()*60*60;
+// 		var minutesInSeconds = myDate.getMinutes()*60;
+// 		var seconds = myDate.getSeconds() + minutesInSeconds + hoursInSeconds;
 
-//time is sped up x10 
-		var percentage = seconds/86400 * 10;
-		console.log(percentage)
+// //time is sped up x10 
+// 		var percentage = seconds;
+// 		console.log(percentage)
+		
 
-//body.style.marginBottom = Math.round(timesTotalHeight*percentage) + "px"; ? 
-body.style.marginBottom = Math.round(timesTotalHeight*percentage) + "px";
-	// progress.style.height = Math.round(timesTotalHeight * percentage) + "px";
+// 		times.style.height =  Math.round(timesTotalHeight * percentage) + "px";
+
+// 	}, 1000)
 
 
-	}, 1000)
+let times = document.getElementById("times")
 
+// Delay time loading so it can run time check function first
+setTimeout(function() {
+  times.style.opacity = '1';
+}, 1000);
+
+// Round time to see what quarter hour we are nearest
+let roundTime = (hours, minutes, minutesToRound) => {
+
+  // Convert hours and minutes to minutes
+  time = (hours * 60) + minutes;
+  let rounded = Math.round(time / minutesToRound) * minutesToRound;
+
+  let roundedHours = Math.floor(rounded / 60)
+  let roundedMinutes = rounded % 60
+
+  return (roundedHours + ':' + roundedMinutes)
+}
+
+// Checks time, changes 'times' position
+let changeTime = () => {
+  let d = new Date(),
+    hours = d.getHours(),
+    minutes = d.getMinutes();
+
+  // Round time to 15 minutes using function above
+  let roundedTime = roundTime(hours, minutes, 15)
+
+  // Find element that matches the current time
+  let currentTime = document.querySelectorAll("[data-time='" + roundedTime + "']")[0]
+
+  if (!!currentTime) {
+    // Get position of current time element
+    let topPos = currentTime.offsetTop
+
+    // Set position of times element from above position
+    times.style.transform = "translateY(-" + topPos + "px)";
+  }
+}
+
+// Run above on page load
+changeTime();
+// Create interval to only run every 10 seconds
+setInterval(changeTime, 10000);
