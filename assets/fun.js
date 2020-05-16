6261718fcbe192e4958d35969f8662befd756f96
Erin McGee <claue312@newschool.edu>
2020-05-15 20:36:21 -0700

    Organize Your Life

--- assets/fun.js
+++ assets/fun.js
@@ -6,3 +6,8 @@ function myTimer() {
   var d = new Date();
   document.getElementById("clock").innerHTML = d.toLocaleTimeString();
 }
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
